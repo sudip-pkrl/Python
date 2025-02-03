09898cffd61b19d8b639fc9e45b7a0f534c54fbb
Sudip Pokhrel <155386506+sudip-pkrl@users.noreply.github.com>
2025-02-03 22:30:50 +0545

    Add files via upload

---
+++ Python_Poem.py
@@ -0,0 +1,17 @@
+def python_poem():
+    verses = [
+        "\n",
+        "Python reigns, Neat and sweet",
+        "No semicolons, Syntax fleet",
+        "Debugging queen, Functions with grace",
+        "Libraries vast, Conquer space",
+        "Data flows, Logic grows",
+        "Jupyter guides, Errors hide",
+        "Python's might, Code shines bright",
+        "Python's tale, We'll regale",
+        "\n"
+    ]
+    for verse in verses:
+        print(verse)
+python_poem() 
+                                      #sudippkrl
