652de0cbe5c3cffcac4d65c419d415d16194c420
Sudip Pokhrel <155386506+sudip-pkrl@users.noreply.github.com>
2025-08-06 22:59:04 +0545

    Add files via upload

---
+++ Examples/E02_Factorial_Recursion.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+if num < 0:
+    print("Factorial does not exist for negative numbers.")
+else:
+    print("The factorial of", num, "is", factorial(num))
